923eaab9d6822baa370272b38c2648bcab2524d2
younghwan314 <younghwan314@naver.com>
2025-02-12 19:38:41 +0900

    init commit

---
+++ src/test/java/com/example/memo2/Memo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.memo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Memo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
